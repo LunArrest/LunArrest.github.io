efe8b763efb1b487d5a174f549b4aca5077254cc
LunArrest <393751812@qq.com>
2015-01-20 17:16:11 +0800

    + 注释

--- revert_python3_config.bat
+++ revert_python3_config.bat
@@ -1,3 +1,4 @@
+REM 该文件直接执行貌似是无效的，要在cmd中直接执行以下命令，原因暂时未知。
 ftype Python.CompiledFile="C:\Python34\python.exe" "%1" %*   
 ftype Python.File="C:\Python34\python.exe" "%1" %*   
 ftype Python.NoConFile="C:\Python34\pythonw.exe" "%1" %*  
