7a681bfc97e6f65991ede7960e694f84eb3cc786
LunArrest <393751812@qq.com>
2015-05-04 09:35:50 +0800

    - python2的路径修改

--- bundle_exec_jekyll_serve.bat
+++ bundle_exec_jekyll_serve.bat
@@ -1,6 +1,6 @@
-set PYTHON2_HOME=C:\Python27
+set PYTHON2_HOME=D:\Python27
 set PATH=%PYTHON2_HOME%\;%PYTHON2_HOME%\Scripts;%PYTHON2_HOME%\Tools\Scripts;%PATH%
-ftype Python.CompiledFile="C:\Python27\python.exe" "%%1" %%*   
-ftype Python.File="C:\Python27\python.exe" "%%1" %%*   
-ftype Python.NoConFile="C:\Python27\pythonw.exe" "%%1" %%*  
+ftype Python.CompiledFile="D:\Python27\python.exe" "%%1" %%*   
+ftype Python.File="D:\Python27\python.exe" "%%1" %%*   
+ftype Python.NoConFile="D:\Python27\pythonw.exe" "%%1" %%*  
 bundle exec jekyll serve
--- revert_python3_config.bat
+++ revert_python3_config.bat
@@ -1,9 +1,9 @@
 ﻿REM 该文件直接执行貌似是无效的，要在cmd中直接执行以下命令，原因暂时未知。
 REM ftype Python.CompiledFile="C:\Python34\python.exe" "%1" %*   
-REM ftype Python.File="C:\Python34\python.exe" "%1" %*   
-REM ftype Python.NoConFile="C:\Python34\pythonw.exe" "%1" %*  
+REM ftype Python.File="D:\Python34\python.exe" "%1" %*   
+REM ftype Python.NoConFile="D:\Python34\pythonw.exe" "%1" %*  
 REM % 为转义字符
 
-ftype Python.CompiledFile="C:\Python34\python.exe" "%%1" %%*   
-ftype Python.File="C:\Python34\python.exe" "%%1" %%*   
-ftype Python.NoConFile="C:\Python34\pythonw.exe" "%%1" %%*  
+ftype Python.CompiledFile="D:\Python34\python.exe" "%%1" %%*   
+ftype Python.File="D:\Python34\python.exe" "%%1" %%*   
+ftype Python.NoConFile="D:\Python34\pythonw.exe" "%%1" %%*  
